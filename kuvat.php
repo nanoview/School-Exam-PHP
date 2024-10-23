61fe97eb57e6e7b55b4d88ab2508adbbd2c865ca
nanoview <arif.js@gmail.com>
2024-10-23 13:19:30 +0300

    form upadated and can send data to database

--- kuvat.php
+++ kuvat.php
@@ -1,44 +1,47 @@
 <!DOCTYPE html>
 <html>
-  <title>
-    Ruoka Express
-  </title>
-     <head>
-          <meta charset="utf-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <link href="https://fonts.googleapis.com/css?family=Spirax&display=swap" rel="stylesheet">
-          <link rel="stylesheet" type="text/css" href="css/style.css">
-          <script type="text/javascript" src="time.js"></script>
-          <script src="https://code.jquery.com/jquery-latest.js"></script>
-     </head>
- <body>
- 
- <?php include 'header.php'; ?>
+<title>
+  Ruoka Express
+</title>
 
-<br><br><br><br>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://fonts.googleapis.com/css?family=Spirax&display=swap" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <script type="text/javascript" src="time.js"></script>
+  <script src="https://code.jquery.com/jquery-latest.js"></script>
+</head>
 
-<div class="row">
-<div class="col-2 col-s-12"></div>
+<body>
 
-<div class="col-8 col-s-12">
+  <?php include 'header.php'; ?>
 
-<?php include 'slideshow.php'; ?> 
+  <br><br><br><br>
 
-</div>
+  <div class="row">
+    <div class="col-2 col-s-12"></div>
 
+    <div class="col-8 col-s-12">
 
-    <div class="col-2 col-s-12">
-            
+      <?php include 'slideshow.php'; ?>
+
     </div>
 
 
- <div class="footer">
- 
-   </div>
+    <div class="col-2 col-s-12">
+
+    </div>
+
+
+    <div class="footer">
+
+    </div>
 
 
 
 
 
- </body>
+</body>
+
 </html>
